214c5adc0df0c6e8a7f3faa348901d1b7b535cc7
Eugene <khemikal2016@gmail.com>
2023-11-30 11:27:03 +0000

    routes

--- school/urls.py
+++ school/urls.py
@@ -5,4 +5,11 @@ urlpatterns = [
     path('get_departments/', get_departments),
     path('add_department/', create_department),
     path('update_department/', update_department),
+    path('get_employees/', get_employees),
+    path('add_employee/', add_employee),
+    path('update_employee/', update_employee),
+    path('get_subjects/', get_subjects),
+    path('add_subject/', add_subjects),
+    path('update_subject/', update_subjects),
+    
 ]
